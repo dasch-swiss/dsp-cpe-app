9fc794a15293e5f087aa62f47d4c79b275380c33
Ivan Subotic <400790+subotic@users.noreply.github.com>
2021-10-27 16:00:01 +0200

    add example spa and gh actions

---
+++ vars.mk
@@ -0,0 +1,12 @@
+DOCKER_REPO := daschswiss/dsp-cpe-app
+
+ifeq ($(BUILD_TAG),)
+	BUILD_TAG := $(shell git describe --tag --dirty --abbrev=7)
+endif
+ifeq ($(BUILD_TAG),)
+	BUILD_TAG := $(shell git rev-parse --verify HEAD)
+endif
+
+ifeq ($(DOCKER_IMAGE),)
+	DOCKER_IMAGE := $(DOCKER_REPO):$(BUILD_TAG)
+endif
