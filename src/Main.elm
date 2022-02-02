3bd2315e05f523e1d60fca88ab006c3d6fe8445e
mdelez <60604010+mdelez@users.noreply.github.com>
2022-02-02 16:40:10 +0100

    chore(primary-button): minor refactoring

--- src/Main.elm
+++ src/Main.elm
@@ -23,11 +23,13 @@ init _ =
 view: Model -> Html msg
 view _ =
     div [class "buttons"]
-        [ div [] [ PrimaryButton.view ( { size = PrimaryButton.ExtraSmall } ) ]
-        , div [] [ PrimaryButton.view ( { size = PrimaryButton.Small } ) ]
-        , div [] [ PrimaryButton.view ( { size = PrimaryButton.Normal } ) ]
-        , div [] [ PrimaryButton.view ( { size = PrimaryButton.Large } ) ]
-        , div [] [ PrimaryButton.view ( { size = PrimaryButton.ExtraLarge } ) ]
+        [ div []
+            [ PrimaryButton.view ( { size = PrimaryButton.ExtraSmall } )
+            , PrimaryButton.view ( { size = PrimaryButton.Small } )
+            , PrimaryButton.view ( { size = PrimaryButton.Normal } )
+            , PrimaryButton.view ( { size = PrimaryButton.Large } )
+            , PrimaryButton.view ( { size = PrimaryButton.ExtraLarge } )
+            ]
         ]
 
 update: msg -> Model -> (Model, Cmd msg)
