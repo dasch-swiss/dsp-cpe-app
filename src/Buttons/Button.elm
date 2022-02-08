fdba5b4515a9d1e7d4fbe1dcc835c6b4293e03e4
André Kilchenmann <github@milchkannen.ch>
2022-02-08 10:49:47 +0100

    feat(button): add circular button

--- src/Buttons/Button.elm
+++ src/Buttons/Button.elm
@@ -1,10 +1,9 @@
-module Button exposing (..)
+module Buttons.Button exposing (..)
 
 import Html exposing (Html)
 import Buttons.CircularButton as CircularButton
-import Buttons.Types exposing (Size)
+import Buttons.Shared exposing (Size(..))
 
 circular : Size -> String -> Html msg
 circular size icon =  
-    CircularButton.view size icon
- 
+    CircularButton.view {size = size, icon = icon}
--- src/TailwindPlayground.elm
+++ src/TailwindPlayground.elm
@@ -2,8 +2,9 @@ module TailwindPlayground exposing (..)
 
 import Browser
 import Buttons.PrimaryButton as PrimaryButton
+import Buttons.CircularButton as CircularButton
 import Buttons.Shared
-import Html exposing (Html, div, p, text)
+import Html exposing (Html, div, h3, text)
 import Html.Attributes exposing (class)
 
 
@@ -30,14 +31,21 @@ view : Model -> Html msg
 view _ =
     div [ class "buttons" ]
         [ div [ class "preview primary-button" ]
-            [ p [ class "header" ] [ text "Primary Buttons" ]
+            [ h3 [ class "header" ] [ text "Primary Buttons" ]
             , PrimaryButton.view { size = Buttons.Shared.ExtraSmall, text = "Extra Small" }
             , PrimaryButton.view { size = Buttons.Shared.Small, text = "Small" }
             , PrimaryButton.view { size = Buttons.Shared.Normal, text = "Normal" }
             , PrimaryButton.view { size = Buttons.Shared.Large, text = "Large" }
             , PrimaryButton.view { size = Buttons.Shared.ExtraLarge, text = "Extra Large" }
             ]
-
+        , div [ class "preview circular-button" ]
+            [ h3 [ class "header" ] [ text "Circular Buttons" ]
+            , CircularButton.view { size = Buttons.Shared.ExtraSmall, icon = "annotation" }
+            , CircularButton.view { size = Buttons.Shared.Small, icon = "arrow-right" }
+            , CircularButton.view { size = Buttons.Shared.Normal, icon = "plus" }
+            , CircularButton.view { size = Buttons.Shared.Large, icon = "check" }
+            , CircularButton.view { size = Buttons.Shared.ExtraLarge, icon = "emojy-happy" }
+            ]
         -- insert other buttons here
         ]
 
