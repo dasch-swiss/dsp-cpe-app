4460dcedbb7cf3467a8cd723dfdbf0c84d30dc87
EricSommerhalder <38506556+EricSommerhalder@users.noreply.github.com>
2022-02-10 17:48:55 +0100

    feat: Circular Avatar

--- src/TailwindPlayground.elm
+++ src/TailwindPlayground.elm
@@ -1,9 +1,9 @@
 module TailwindPlayground exposing (..)
 import Browser
 import Buttons.Button exposing (..)
-import Html exposing (Html, div, p, text)
+import Html exposing (Html, h3, div, p, text)
 import Html.Attributes exposing (class)
-
+import Avatars.CircularAvatar as CircularAvatar
 
 type alias Model =
     String
@@ -26,35 +26,57 @@ init _ =
 
 view : Model -> Html msg
 view _ =
-    div [ class "buttons" ]
-        [ div [ class "preview primary-button" ]
-            [ p [ class "header" ] [ text "Primary Buttons" ]
-            , cpeButton (Primary, ExtraSmall, "Extra small")
-            , cpeButton (Primary, Small, "Small")
-            , cpeButton (Primary, Normal, "Normal")
-            , cpeButton (Primary, Large, "Large")
-            , cpeButton (Primary, ExtraLarge, "Extra large")
-            ]
+    div [ class "main"]
+        [ div [ class "buttons" ]
+            [ div [ class "preview primary-button" ]
+                [ p [ class "header" ] [ text "Primary Buttons" ]
+                , cpeButton (Primary, ExtraSmall, "Extra small")
+                , cpeButton (Primary, Small, "Small")
+                , cpeButton (Primary, Normal, "Normal")
+                , cpeButton (Primary, Large, "Large")
+                , cpeButton (Primary, ExtraLarge, "Extra large")
+                ]
 
-        , div [ class "preview secondary-button" ]
-            [ p [ class "header" ] [ text "Secondary Buttons" ]
-            , cpeButton (Secondary, ExtraSmall, "Extra small")
-            , cpeButton (Secondary, Small, "Small")
-            , cpeButton (Secondary, Normal, "Normal")
-            , cpeButton (Secondary, Large, "Large")
-            , cpeButton (Secondary, ExtraLarge, "Extra large")
-            ]
-        , div [ class "preview white-button" ]
-        [ p [ class "header" ] [ text "White Buttons" ]
-            , cpeButton (White, ExtraSmall, "Extra small")
-            , cpeButton (White, Small, "Small")
-            , cpeButton (White, Normal, "Normal")
-            , cpeButton (White, Large, "Large")
-            , cpeButton (White, ExtraLarge, "Extra large")
-        ] 
+            , div [ class "preview secondary-button" ]
+                [ p [ class "header" ] [ text "Secondary Buttons" ]
+                , cpeButton (Secondary, ExtraSmall, "Extra small")
+                , cpeButton (Secondary, Small, "Small")
+                , cpeButton (Secondary, Normal, "Normal")
+                , cpeButton (Secondary, Large, "Large")
+                , cpeButton (Secondary, ExtraLarge, "Extra large")
+                ]
+            , div [ class "preview white-button" ]
+            [ p [ class "header" ] [ text "White Buttons" ]
+                , cpeButton (White, ExtraSmall, "Extra small")
+                , cpeButton (White, Small, "Small")
+                , cpeButton (White, Normal, "Normal")
+                , cpeButton (White, Large, "Large")
+                , cpeButton (White, ExtraLarge, "Extra large")
+            ] 
 
-        -- insert other buttons here
+            -- insert other buttons here
         ]
+        , div [class "avatars"]
+            [ div [ class "preview circular-avatar"]
+                [ h3 [ class "header"][ text "Circular Avatar"]
+                    , CircularAvatar.view { size = CircularAvatar.ExtraSmall
+                        , img = "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                        , alt = "Extra small"}
+                    , CircularAvatar.view { size = CircularAvatar.Small
+                        , img = "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                        , alt = "Small"}
+                    , CircularAvatar.view { size = CircularAvatar.Normal
+                        , img = "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                        , alt = "Normal"}
+                    , CircularAvatar.view { size = CircularAvatar.Large
+                        , img = "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                        , alt = "Large"}
+                    , CircularAvatar.view { size = CircularAvatar.ExtraLarge
+                        , img = "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                        , alt = "Extra large"}
+                ]
+        ]
+    ]
 
 type Msg = DisplayWhichButtonClicked 
 
