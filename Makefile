14aa7510aa5e368b61b731adc309e9a00c5dfbbd
EricSommerhalder <38506556+EricSommerhalder@users.noreply.github.com>
2022-02-18 18:02:25 +0100

    feat: Added make command

--- Makefile
+++ Makefile
@@ -40,4 +40,8 @@ clean: ## delete elm and node dependencies
 help: ## this help
 	@awk 'BEGIN {FS = ":.*?## "} /^[a-zA-Z_-]+:.*?## / {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}' $(MAKEFILE_LIST) | sort
 
+.PHONY: heroicons
+heroicons: ## run the script to update the heroicons library
+	python get-heroicons-list.py
+
 .DEFAULT_GOAL := help
--- get-heroicons-list.py
+++ get-heroicons-list.py
@@ -6,7 +6,7 @@ icons = []
 for line in arr :
     if not line.find("->") == -1 and not line.find('Html msg') == -1:
         icons.append(line[0:line.find(' ')])
-to_write = "module Buttons.Icon exposing (..)\nimport Heroicons.Solid\nimport Html exposing (Html)\n\ntype Icon\n    = "
+to_write = "module Icon exposing (..)\nimport Heroicons.Solid\nimport Html exposing (Html)\n\ntype Icon\n    = "
 for icon in icons:
     to_write += icon.capitalize() + '\n    | '
 to_write = to_write[0:-6] + '\n\ngetHtml: Icon -> Html msg\ngetHtml icon =\n    case icon of\n'
