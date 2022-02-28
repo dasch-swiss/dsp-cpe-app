ee5e8d63bcecf5df11be4821c849ccf3a556e158
User1 <dominique.steinbach@unibas.ch>
2022-02-28 15:21:23 +0100

    reduced WhiteButton  to the minimal definition of variant specific tw/css.

---
+++ src/Buttons/BasicButtons/WhiteButton.elm
@@ -0,0 +1,13 @@
+module Buttons.BasicButtons.WhiteButton exposing (..)
+import Css
+import Tailwind.Utilities as Tw
+
+
+whiteButtonStyle : List Css.Style
+whiteButtonStyle =
+    [ Tw.text_gray_700
+    , Tw.bg_white
+    , Tw.border_gray_300
+    , Css.hover
+        [ Tw.bg_gray_50 ]
+    ]
