99031d0bf7c2fe17604929119c2d9ea8ad0871c1
domsteinbach <36757218+domsteinbach@users.noreply.github.com>
2022-03-29 07:56:22 +0200

    refactor: removes Browser.Program() with init, view, update;

--- src/Dividers/IconButtonDivider.elm
+++ src/Dividers/IconButtonDivider.elm
@@ -1,7 +1,6 @@
 module Dividers.IconButtonDivider exposing (..)
 
-import Browser
-import Buttons.Button exposing (divider)
+import Buttons.Button exposing (dividerButton)
 import Buttons.LeadingIconButton exposing (LeadingSize(..))
 import Html exposing (Html)
 import Html.Styled as HtmlStyled exposing (Attribute)
@@ -10,34 +9,11 @@ import Icon
 import Tailwind.Utilities as Tw
 
 
-main : Program () (Model msg) msg
-main =
-    Browser.element
-        { init = init
-        , view = view
-        , update = update
-        , subscriptions = subscriptions
-        }
-
-
 type alias Model msg =
     { buttonAttrs : List (Attribute msg)
     , icon : Icon.Icon
     , text : String
     }
-
-
-initialModel : Model msg
-initialModel =
-    { buttonAttrs = []
-    , icon = Icon.PlusSm
-    , text = ""
-    }
-
-
-init : () -> ( Model msg, Cmd msg )
-init _ =
-    ( initialModel, Cmd.none )
 
 
 view : Model msg -> Html msg
@@ -50,14 +26,5 @@ view model =
                 [ Attr.css [ Tw.absolute, Tw.inset_0, Tw.flex, Tw.items_center ] ]
                 [ HtmlStyled.div [ Attr.css [ Tw.w_full, Tw.border_t, Tw.border_gray_300 ] ] [] ]
             , HtmlStyled.div [ Attr.css [ Tw.relative, Tw.flex, Tw.justify_center ] ]
-                [ HtmlStyled.fromUnstyled (divider model.buttonAttrs model.text model.icon) ]
+                [ dividerButton model.buttonAttrs model.text model.icon ]
             ]
-
-
-update : msg -> Model msg -> ( Model msg, Cmd msg )
-update _ model =
-    ( model, Cmd.none )
-
-
-subscriptions _ =
-    Sub.none
