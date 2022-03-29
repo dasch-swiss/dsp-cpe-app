a73c2a427010eb37ff4a94a9b57821774717e4dc
domsteinbach <36757218+domsteinbach@users.noreply.github.com>
2022-03-29 08:01:12 +0200

    enhancement: uses the new functional and unstyled buttons

--- tests/ButtonsTest.elm
+++ tests/ButtonsTest.elm
@@ -1,54 +1,66 @@
 module ButtonsTest exposing (..)
 
+import Buttons.BasicButtons.BasicButton exposing (BasicButtonSize(..))
+import Buttons.Button as Button exposing (primaryButton, secondaryButton, whiteButton)
+import Buttons.CircularButton exposing (CircularButtonSize(..))
 import Expect
+import Html exposing (Html)
+import Icon
 import Test exposing (..)
 import Test.Html.Query as Query
-import Buttons.Button as Button
-import Buttons.Button exposing (primaryButton, secondaryButton)
-import Html exposing (Html)
 import Test.Html.Selector
-import Icon
-import Buttons.Button exposing (whiteButton)
-import Buttons.BasicButtons.BasicButton exposing (BasicButtonSize(..))
-import Buttons.CircularButton exposing (CircularButtonSize(..))
-
 
 
 primary : Html msg
-primary  = primaryButton [] "Primary Button" BasicExtraSmall
+primary =
+    Button.primaryButtonUnStyled [] "Primary Button" BasicExtraSmall
+
+
 primaryCreated : Test
 primaryCreated =
     test "One 'Normal' sized primary button should be created via the initialModel." <|
         \_ ->
             primary
                 |> Query.fromHtml
-                |> Query.findAll [ Test.Html.Selector.text "Primary Button"]
+                |> Query.findAll [ Test.Html.Selector.text "Primary Button" ]
                 |> Query.count (Expect.equal 1)
 
+
 secondary : Html msg
-secondary  = secondaryButton [] "Secondary Button" BasicExtraSmall
+secondary =
+    Button.secondaryButtonUnStyled [] "Secondary Button" BasicExtraSmall
+
+
 secondaryCreated : Test
 secondaryCreated =
     test "One 'ExtraSmall' sized secondary button should be created via the initialModel." <|
         \_ ->
             secondary
                 |> Query.fromHtml
-                |> Query.findAll [ Test.Html.Selector.text "Secondary Button"]
+                |> Query.findAll [ Test.Html.Selector.text "Secondary Button" ]
                 |> Query.count (Expect.equal 1)
 
+
 white : Html msg
-white  = whiteButton[] "White Button" BasicExtraSmall
+white =
+    Button.whiteButtonUnstyled [] "White Button" BasicExtraSmall
+
+
 whiteCreated : Test
 whiteCreated =
     test "One 'ExtraLarge' sized white button should be created via the initialModel." <|
         \_ ->
             white
                 |> Query.fromHtml
-                |> Query.findAll [ Test.Html.Selector.text "White Button"]
+                |> Query.findAll [ Test.Html.Selector.text "White Button" ]
                 |> Query.count (Expect.equal 1)
 
+
 circular : Html msg
-circular  = Button.circular (CircularLarge, Icon.AcademicCap)
+circular =
+    Button.circularButtonUnstyled [] Icon.AcademicCap CircularLarge
+
+
 circularCreated : Test
 circularCreated =
     test "One 'Large' sized circular button should be created via the initialModel." <|
