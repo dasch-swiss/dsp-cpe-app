a3b6e2bbc55dfda5b3d5f2afb918526c8def3069
domsteinbach <36757218+domsteinbach@users.noreply.github.com>
2022-04-06 16:40:25 +0200

    refactor (css): redesign headerbuttons

--- src/NavigationHeader/HeaderButtons.elm
+++ src/NavigationHeader/HeaderButtons.elm
@@ -1,11 +1,9 @@
 module NavigationHeader.HeaderButtons exposing (signInButton, signUpButton)
 
-import Css exposing (backgroundColor, border, borderColor, borderStyle, px, solid)
-import Css.Global
-import Html.Styled as HtmlStyled exposing (Attribute, Html)
-import Html.Styled.Attributes as Attr
-import NavigationHeader.CssColors exposing (primary, white)
-import Tailwind.Utilities as Tw
+import CustomCss.CssColors exposing (primary, white)
+import CustomCss.DaschTailwind as Dtw
+import Html exposing (Attribute, Html)
+import Html.Attributes as Attr exposing (class)
 
 
 signUpButton : List (Attribute msg) -> String -> Html msg
@@ -18,58 +16,63 @@ signInButton attrs txt =
     view attrs txt signInStyle
 
 
-view : List (Attribute msg) -> String -> List Css.Style -> Html msg
+view : List (Attribute msg) -> String -> Attribute msg -> Html msg
 view attrs txt style =
-    HtmlStyled.button
+    Html.button
         (attrs
             ++ [ Attr.type_ "button"
-               , Attr.css style
+               , style
                ]
         )
-        [ HtmlStyled.text txt
-        , Css.Global.global Tw.globalStyles
+        [ Html.text txt
         ]
 
 
-signUpStyle : List Css.Style
+signUpStyle : Attribute msg
 signUpStyle =
-    [ Tw.px_4
-    , Tw.py_2
-    , Tw.inline_flex
-    , Tw.items_center
-    , Tw.justify_center
-    , backgroundColor white
-    , border (px 1)
-    , borderColor primary
-    , borderStyle solid
-    , Tw.rounded
-    , Tw.shadow_sm
-    , Css.focus
-        [ Tw.outline_none
-        , Tw.ring_2
-        , Tw.ring_inset
+    [ Dtw.px_4
+    , Dtw.py_2
+    , Dtw.inline_flex
+    , Dtw.items_center
+    , Dtw.justify_center
+    , Dtw.bg_white
+    , Dtw.border
+    , Dtw.rounded
+    , Dtw.shadow_sm
+    , Dtw.onFocus
+        [ Dtw.outline_none
+        , Dtw.ring_2
+        , Dtw.ring_inset
         ]
-    , Css.color primary
     ]
+        |> Dtw.dtwClass
 
 
-signInStyle : List Css.Style
+
+--     , Css.color primary
+
+
+signInStyle : Attribute msg
 signInStyle =
-    [ Tw.inline_flex
-    , Tw.items_center
-    , Tw.justify_center
-    , Tw.px_4
-    , Tw.py_2
-    , Css.backgroundColor primary
-    , border (px 1)
-    , borderColor primary
-    , borderStyle solid
-    , Tw.rounded
-    , Tw.shadow_sm
-    , Tw.text_white
-    , Css.focus
-        [ Tw.outline_none
-        , Tw.ring_2
-        , Tw.ring_inset
+    [ Dtw.inline_flex
+    , Dtw.items_center
+    , Dtw.justify_center
+    , Dtw.px_4
+    , Dtw.py_2
+    , Dtw.border
+    , Dtw.rounded
+    , Dtw.shadow_sm
+    , Dtw.bg_medium_blue_700
+    , Dtw.text_white
+    , Dtw.onFocus
+        [ Dtw.outline_none
+        , Dtw.ring_2
+        , Dtw.ring_inset
         ]
     ]
+        |> Dtw.dtwClass
+
+
+
+-- Css.backgroundColor primary
+--    , borderColor primary
