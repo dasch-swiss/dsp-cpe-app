84f1769cd3c6c894a33ffbd237cb92dac4b8a1bd
mdelez <60604010+mdelez@users.noreply.github.com>
2022-06-07 16:11:36 +0200

    chore: fix merge conflict

--- src/Modules/Projects/TailwindPlayground.elm
+++ src/Modules/Projects/TailwindPlayground.elm
@@ -309,17 +309,3 @@ exampleProjectDescription =
     , subtitle = "Subtitle"
     , isOpen = False
     }
-
-
-exampleProjectDescription : ProjectDescription.Model
-exampleProjectDescription =
-    { text = """Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus 
-                    est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, 
-                    no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. 
-                    Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, 
-                    vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod 
-                    tincidunt ut laoreet dolore magna aliquam erat volutpat."""
-    , title = "Title"
-    , subtitle = "Subtitle"
-    , isOpen = False
-    }
